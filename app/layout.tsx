d77abe7c43f5d4ff190aa30bdd41165a4ba6795b
Eunji <eunjipark0129@gmail.com>
2025-04-14 23:35:29 +0900

    [chore/#6] css 초기 설정

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const NotoSans = localFont({
+    src: [
+        {
+            path: "../public/fonts/NotoSansKR-Bold.woff",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/NotoSansKR-SemiBold.woff",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/NotoSansKR-Regular.woff",
+            weight: "400",
+            style: "normal",
+        },
+    ],
+});
+
+export const metadata: Metadata = {
+    title: "Change Me",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="ko" className={NotoSans.className}>
+            <body>{children}</body>
+        </html>
+    );
+}
