55e7e5cbc96d7bf6fa96ed1919d39889a49a8750
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-16 14:46:38 +0900

    [feat/#47] api(GET) fetch 구현

---
+++ app/message/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { MessageDto } from "@/application/usecase/message/dto/MessageDto";
+import { MessageListDto } from "@/application/usecase/message/dto/MessageListDto";
+import { useMemberStore } from "@/stores/memberStore";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function MessageListPage() {
+    const searchParams = useSearchParams();
+
+    const currentPageParam = searchParams.get("currentPage");
+    const mineParam = searchParams.get("mine");
+
+    const { token } = useMemberStore();
+
+    // page state initialization
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [mine, setMine] = useState<boolean>(false);
+    const [messages, setMessages] = useState<MessageDto[]>([]);
+    const [pages, setPages] = useState<number[]>([]);
+
+    // query params initialization
+    if (currentPageParam) setCurrentPage(Number(currentPageParam));
+    if (mineParam) setMine(mineParam === "true");
+
+    useEffect(() => {
+        async function fetchMessages() {
+            try {
+                const params = new URLSearchParams();
+
+                // append query parameters to params
+                params.append("currentPage", currentPage.toString());
+                params.append("mine", mine.toString());
+
+                console.log("params", params.toString());
+
+                // call API
+                const response = await fetch(
+                    `/api/messages?${params.toString()}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    },
+                );
+
+                const data: MessageListDto = await response.json();
+                console.log("data", data);
+
+                // update state with the fetched data
+                setMessages(data.messages);
+                setPages(data.pages);
+            } catch (error) {
+                console.error("Failed to fetch messages:", error);
+            }
+        }
+
+        fetchMessages();
+    }, [currentPage, mine, token]);
+
+    return {};
+}
