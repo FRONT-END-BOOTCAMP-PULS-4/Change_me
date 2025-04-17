2de1f99718c646faa30f4a73b8a4ba07fb47e12f
Eunji <eunjipark0129@gmail.com>
2025-04-17 09:32:30 +0900

    [feat/#56] 카테고리 수정 기능

---
+++ application/usecase/category/UpdateCategoryUsecase.ts
@@ -0,0 +1,22 @@
+import { CategoryRepository } from "@/domain/repositories/CategoryRepository";
+import { UpdateCategoryDto } from "./dto/UpdateCategoryDto";
+import { CategoryView } from "@/domain/entities/CategoryView";
+
+export class UpdateCategoryUsecase {
+    constructor(private categoryRepository: CategoryRepository) {}
+
+    async execute(updateDto: UpdateCategoryDto) {
+        const category: CategoryView = await this.categoryRepository.findById(
+            updateDto.id,
+        );
+        console.log(category);
+        if (category.habitCount !== 0) {
+            throw new Error("사용량이 0인 카테고리만 수정 가능합니다.");
+        }
+
+        await this.categoryRepository.update({
+            id: updateDto.id,
+            name: updateDto.name,
+        });
+    }
+}
--- domain/repositories/CategoryRepository.ts
+++ domain/repositories/CategoryRepository.ts
@@ -1,10 +1,12 @@
 import { Category } from "../entities/Category";
+import { CategoryView } from "../entities/CategoryView";
 import { CategoryFilter } from "./filters/CategoryFilter";
 
 export interface CategoryRepository {
     count(filter?: CategoryFilter): Promise<number>;
     findAll(filter?: CategoryFilter): Promise<Category[]>;
+    findById(id: number): Promise<CategoryView>;
     save(category: Category): Promise<Category>;
-    update(category: Category): Promise<Category>;
+    update(category: Category): Promise<void>;
     deleteById(id: number): Promise<void>;
 }
