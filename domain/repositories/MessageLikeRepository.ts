24dce04dbfae0b46b6e9e560e692214e1fbed4b8
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-17 16:17:22 +0900

    [feat/#78] 메시지 좋아요 기능 관련 Repo, SbRepo 수정 update, findById 등은 사용할 이유가 없어서 제거함.

--- domain/repositories/MessageLikeRepository.ts
+++ domain/repositories/MessageLikeRepository.ts
@@ -4,8 +4,6 @@ import { MessageLikeFilter } from "./filters/MessageLikeFilter";
 export interface MessageLikeRepository {
     count(filter?: MessageLikeFilter): Promise<number>;
     findAll(filter?: MessageLikeFilter): Promise<MessageLike[]>;
-    findById(id: number): Promise<MessageLike | null>;
     save(messageLike: MessageLike): Promise<MessageLike>;
-    update(messageLike: MessageLike): Promise<MessageLike>;
-    deleteById(id: number): Promise<void>;
+    delete(messageLike: MessageLike): Promise<void>;
 }
