210616523a5433c95041c8ad93ae954e7fde4b39
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-18 17:50:20 +0900

    [feat/#101] Message 생성 구현중

--- application/usecase/message/CreateMessageUsecase.ts
+++ application/usecase/message/CreateMessageUsecase.ts
@@ -1,12 +1,24 @@
-export class CreateMessageUsecase {
-    constructor(
-        public memberId: string,
-        public content: string,
-        public createdAt: Date = new Date(),
-    ) {}
+import { MessageRepository } from "@/domain/repositories/MessageRepository";
+import { CreateMessageDto } from "./dto/CreateMessageDto";
+import { Message } from "@/domain/entities/Message";
 
-    execute() {
-        // Logic to create a new message
-        // This could involve calling a repository method to save the message to a database
+export class CreateMessageUsecase {
+    private messageRepository: MessageRepository;
+
+    constructor(messageRepository: MessageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    async execute(createMessageDto: CreateMessageDto): Promise<Message> {
+        const message: Message = new Message(
+            null,
+            createMessageDto.memberId,
+            createMessageDto.content,
+            new Date(),
+            null,
+        );
+
+        const newMessage = await this.messageRepository.save(message);
+        return newMessage;
     }
 }
