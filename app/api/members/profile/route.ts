a86baf01d15f3b590a8df5f5a97e895e8b5b7d15
Parkchanwoo <parkchanwoo7@naver.com>
2025-04-19 00:19:45 +0900

    [style/#108] 마이페이지에서 프로필정보 가져오는 부분, 탈퇴하기에서 getMemberIdFromToken 사용하도록 수정

--- app/api/members/profile/route.ts
+++ app/api/members/profile/route.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { GetProfileUseCase } from "@/application/usecase/member/GetProfileUsecase";
 import { SbMemberRepository } from "@/infra/repositories/supabase/SbMemberRepository";
+import { getMemberIdFromToken } from "@/utils/auth";
 
 export async function GET(req: NextRequest) {
     const authHeader = req.headers.get("authorization");
-    const token = authHeader?.replace("Bearer ", "");
+    const memberId = await getMemberIdFromToken(authHeader!);
 
     try {
         const memberRepository = new SbMemberRepository();
         const getProfileUseCase = new GetProfileUseCase(memberRepository);
 
-        const profile = await getProfileUseCase.execute(token!);
+        const profile = await getProfileUseCase.execute(memberId!);
         return NextResponse.json(profile);
     } catch (err) {
         return NextResponse.json({ error: "인증 실패" }, { status: 401 });
--- application/usecase/member/WithdrawMemberUsecase.ts
+++ application/usecase/member/WithdrawMemberUsecase.ts
@@ -4,12 +4,7 @@ import { verifyJWT } from "@/utils/jwt";
 export class WithdrawMemberUseCase {
     constructor(private readonly memberRepository: MemberRepository) { }
 
-    async execute(token: string): Promise<void> {
-        const payload = await verifyJWT(token);
-        if (!payload || typeof payload !== "object" || !("id" in payload)) {
-            throw new Error("인증 실패");
-        }
-
-        await this.memberRepository.withdraw(payload.id as string);
+    async execute(memberId: string): Promise<void> {
+        await this.memberRepository.withdraw(memberId as string);
     }
 }
