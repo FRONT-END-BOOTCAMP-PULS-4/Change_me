b16d027975dd5d0ccc732cbc48f22c90fe2beb52
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-21 15:07:07 +0900

    [fix/#129] 메시지 목록 Pager 수정내용 반영

--- application/usecase/message/dto/MessageListDto.ts
+++ application/usecase/message/dto/MessageListDto.ts
@@ -3,8 +3,8 @@ import { MessageDto } from "./MessageDto";
 export class MessageListDto {
     constructor(
         public messages: MessageDto[],
-        public totalCount: number, // total number of messages
-        public endPage: number, // last page number
+        public currentPage: number, // current page number
         public pages: number[], // array of page numbers to show in pagination
+        public endPage: number, // last page number
     ) {}
 }
