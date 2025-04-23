3accd0d546d145dbacfd17d595719b1fe773f46b
Parkchanwoo <parkchanwoo7@naver.com>
2025-04-23 14:26:48 +0900

    [Feat/#184] 403 페이지 css

--- app/error/403/page.tsx
+++ app/error/403/page.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
+import styles from "./Page403.module.scss";
 
 export default function Page403() {
     return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>403 - Forbidden</h1>
-            <p>죄송합니다. 이 페이지에 접근할 권한이 없습니다.</p>
-            <Link
-                href="/"
-                style={{ color: "blue", textDecoration: "underline" }}
-            >
+        <div className={styles.container}>
+            <Image
+                src="/images/NotFound.png"
+                alt="NotFound"
+                width={140}
+                height={140}
+                className={styles.image}
+            />
+            <h1 className={styles.title}>403 - Forbidden</h1>
+            <p className={styles.message}>죄송합니다. 이 페이지에 접근할 권한이 없습니다.</p>
+            <Link href="/" className={styles.link}>
                 홈으로 돌아가기
             </Link>
         </div>
