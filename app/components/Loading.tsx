750dbcbd0d8b33c8256d7f14423ce8a891d7cf5f
Parkchanwoo <parkchanwoo7@naver.com>
2025-04-23 14:05:16 +0900

    [feat/#181] 로딩페이지 css

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import Image from "next/image";
+import styles from "./Loading.module.scss";
+
 export default function Loading() {
-    return <div>Loading...</div>;
+    return (
+        <div className={styles.container}>
+            <Image
+                src="/images/Loading.png"
+                alt="로딩 중 이미지"
+                width={140}
+                height={140}
+                className={styles.image}
+            />
+            <div className={styles.text}>Loading...</div>
+        </div>
+    );
 }
