f1746db522f0815c86cf0174386aaf0de11109ab
Parkchanwoo <parkchanwoo7@naver.com>
2025-04-23 10:28:34 +0900

    [refactore/#175] 오늘의 루틴페이지에서 습관 체크, 달성률에 따른 상태 변화 api 하나로 통합

--- app/api/test-habit-records/route.ts
+++ app/api/test-habit-records/route.ts
@@ -2,14 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import { TestHabitRecordDto } from "@/application/usecase/habit/dto/TestHabitRecordDto";
 import { TestHabitRecordUsecase } from "@/application/usecase/habit/TestHabitRecordUsecase";
 import { SbHabitRecordRepository } from "@/infra/repositories/supabase/SbHabitRecordRepository";
+import { SbHabitRepository } from "@/infra/repositories/supabase/SbHabitRepository";
+import { TestAutoUpdateHabitStatusUsecase } from "@/application/usecase/habit/TestAutoUpdateHabitStatusUsecase";
 
 export async function POST(req: NextRequest) {
     try {
         const { habitId, date } = await req.json();
 
         const dto = new TestHabitRecordDto(habitId, new Date(date));
+        const recordRepo = new SbHabitRecordRepository();
+        const habitRepo = new SbHabitRepository();
         const usecase = new TestHabitRecordUsecase(new SbHabitRecordRepository());
         const result = await usecase.execute(dto);
+
+        const statusUsecase = new TestAutoUpdateHabitStatusUsecase(habitRepo, recordRepo);
+        await statusUsecase.execute(habitId);
 
         return NextResponse.json({ status: result.status });
     } catch (error: any) {
@@ -22,8 +29,13 @@ export async function DELETE(req: NextRequest) {
         const { habitId, date } = await req.json();
 
         const dto = new TestHabitRecordDto(habitId, new Date(date));
+        const recordRepo = new SbHabitRecordRepository();
+        const habitRepo = new SbHabitRepository();
         const usecase = new TestHabitRecordUsecase(new SbHabitRecordRepository());
         const result = await usecase.execute(dto);
+
+        const statusUsecase = new TestAutoUpdateHabitStatusUsecase(habitRepo, recordRepo);
+        await statusUsecase.execute(habitId);
 
         return NextResponse.json({ status: result.status });
     } catch (error: any) {
