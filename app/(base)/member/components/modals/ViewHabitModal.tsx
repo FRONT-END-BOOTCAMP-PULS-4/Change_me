f4782a46c470e4a863f659d75e896d78b8a7d399
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-24 15:21:16 +0900

    [feat/#206] react-calendar 가져오기

--- app/(base)/member/components/modals/ViewHabitModal.tsx
+++ app/(base)/member/components/modals/ViewHabitModal.tsx
@@ -3,13 +3,25 @@
 import ModalWrapper from "@/app/components/ModalWrapper";
 import useModalStore from "@/stores/modalStore";
 import React from "react";
+import Calendar from "react-calendar";
+import styles from "./ViewHabitModal.module.scss";
+import { useAuthStore } from "@/stores/authStore";
 
-export default function ViewHabitModal() {
+type ViewHabitModalProps = {
+    habitId: number;
+};
+
+export default function ViewHabitModal(props: ViewHabitModalProps) {
     const { isOpen, closeModal } = useModalStore();
+    const { token } = useAuthStore();
 
     return (
         <ModalWrapper isOpen={isOpen} onClose={closeModal}>
-            <div>습관 상세보기</div>
+            <div className={styles.wrapper}>
+                <h2>{}</h2>
+                <div className={styles.description}>{}</div>
+                <Calendar />
+            </div>
         </ModalWrapper>
     );
 }
