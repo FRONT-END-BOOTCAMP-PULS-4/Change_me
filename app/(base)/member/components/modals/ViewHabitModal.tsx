7264f535c17adb42f642c64a2b9fac72a939df06
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-24 16:48:28 +0900

    [feat/#206] 달력 수정중

--- app/(base)/member/components/modals/ViewHabitModal.tsx
+++ app/(base)/member/components/modals/ViewHabitModal.tsx
@@ -2,25 +2,53 @@
 
 import ModalWrapper from "@/app/components/ModalWrapper";
 import useModalStore from "@/stores/modalStore";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import styles from "./ViewHabitModal.module.scss";
 import { useAuthStore } from "@/stores/authStore";
 
 type ViewHabitModalProps = {
     habitId: number;
+    habitName: string;
+    habitDescrition: string;
 };
 
 export default function ViewHabitModal(props: ViewHabitModalProps) {
     const { isOpen, closeModal } = useModalStore();
-    const { token } = useAuthStore();
+
+    const [dates, setDates] = useState<Date[]>([]);
+
+    useEffect(() => {
+        const fetchDates = async () => {
+            try {
+                const res = await fetch(
+                    `/api/members/records/${props.habitId}`,
+                );
+                const data = await res.json();
+                if (Array.isArray(data.dates)) {
+                    setDates(data.dates);
+                }
+            } catch (error) {
+                console.error("습관 기록 불러오기 실패:", error);
+            }
+        };
+        fetchDates();
+    }, []);
 
     return (
         <ModalWrapper isOpen={isOpen} onClose={closeModal}>
             <div className={styles.wrapper}>
-                <h2>{}</h2>
-                <div className={styles.description}>{}</div>
-                <Calendar />
+                <h2>{props.habitName}</h2>
+                <div className={styles.description}>
+                    {props.habitDescrition}
+                </div>
+                <Calendar
+                    formatDay={(locale, date) => moment(date).format("DD")} // should be changed
+                    calendarType="gregory"
+                    prev2Label={null}
+                    next2Label={null}
+                    showNeighboringMonth={false}
+                />
             </div>
         </ModalWrapper>
     );
