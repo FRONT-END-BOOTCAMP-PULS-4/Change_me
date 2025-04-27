5e48b6962c84d4025fedcdef559559daf9f6f527
Kwon Woojin <wojin57@gmail.com>
2025-04-27 13:53:01 +0900

    [feat/#201] 좋아요 상태 UI변화 로컬에서 처리

--- app/(base)/member/message/components/LikeButton.tsx
+++ app/(base)/member/message/components/LikeButton.tsx
@@ -7,34 +7,55 @@ import { MessageLikeDto } from "@/application/usecase/message-like/dto/MessageLi
 import { useMessageLikes } from "@/hooks/useMessageLikes";
 import Image from "next/image";
 import styles from "./LikeButton.module.scss";
+import { memo, useEffect, useState } from "react";
 
 type LikeButtonProps = {
     isLiked: Boolean;
+    likeCount: number;
     messageLikeDto: MessageLikeDto;
 };
 
 export default function LikeButton(props: LikeButtonProps) {
-    const iconImage = props.isLiked ? fullHeartImage : emptyHeartImage;
+    // local state to manage the like status
+    const [liked, setLiked] = useState(props.isLiked);
+    const [count, setCount] = useState(props.likeCount);
+
+    useEffect(() => {
+        {
+            setLiked(props.isLiked);
+            setCount(props.likeCount);
+        }
+    }, []);
     const messageLikes = useMessageLikes();
+
     const toggleLike = async () => {
+        const newLiked = !liked;
+
+        setLiked(newLiked);
+        setCount((prev) => prev + (newLiked ? 1 : -1));
+
         try {
-            if (!props.isLiked) {
-                const res = await messageLikes.createMessageLike(
+            if (newLiked) {
+                await messageLikes.createMessageLike(
                     props.messageLikeDto.messageId,
                 );
-                // update values: instead of using setIsLiked, apply the change by editing local variables..?
             } else {
-                const res = await messageLikes.deleteMessageLike(
+                await messageLikes.deleteMessageLike(
                     props.messageLikeDto.messageId,
                 );
-                // update values
             }
-        } catch (error) {}
+        } catch (error) {
+            console.error(`Error toggling like status: ${error}`);
+            setLiked(liked);
+            setCount((prev) => prev + (newLiked ? -1 : 1));
+        }
     };
+    const iconImage = liked ? fullHeartImage : emptyHeartImage;
 
     return (
         <button className={styles.button} onClick={toggleLike}>
             <Image src={iconImage} alt="하트 아이콘" width={20} height={20} />
+            <div>{count}</div>
         </button>
     );
 }
