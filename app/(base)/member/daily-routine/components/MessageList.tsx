b3a628b37db1ca0bae4f65e6cb4b32b909671205
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-04-28 11:34:01 +0900

    [chore/#287] 메시지 리스트 비어있을 때 메시지 추가

--- app/(base)/member/daily-routine/components/MessageList.tsx
+++ app/(base)/member/daily-routine/components/MessageList.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./MessageList.module.scss";
 import MessageItem from "./MessageItem";
 import { MessageDto } from "@/application/usecase/message/dto/MessageDto";
+import Image from "next/image";
 
 type MessageListProps = {
     messages: MessageDto[];
@@ -14,16 +15,32 @@ export default function MessageList(props: MessageListProps) {
     return (
         <div className={styles.wrapper}>
             <ol className={styles.list}>
-                {props.messages.map((message: MessageDto) => (
-                    <li className={styles.listItem} key={message.id}>
-                        <MessageItem
-                            messageDto={message}
-                            key={message.id}
-                            handleUpdate={props.handleUpdate}
-                            handleDelete={props.handleDelete}
+                {props.messages.length === 0 ? (
+                    <li className={styles.empty}>
+                        <Image
+                            src={"/images/Loading.png"}
+                            alt="함께해요 이미지"
+                            width={200}
+                            height={200}
+                            className={styles.image}
+                            priority={true}
                         />
+                        아직 작성된 메시지가 없어요.
+                        <br />
+                        모두에게 응원의 메시지를 남겨보세요!
                     </li>
-                ))}
+                ) : (
+                    props.messages.map((message: MessageDto) => (
+                        <li className={styles.listItem} key={message.id}>
+                            <MessageItem
+                                messageDto={message}
+                                key={message.id}
+                                handleUpdate={props.handleUpdate}
+                                handleDelete={props.handleDelete}
+                            />
+                        </li>
+                    ))
+                )}
             </ol>
         </div>
     );
